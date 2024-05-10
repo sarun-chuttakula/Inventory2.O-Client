50325168c93b55f0d755a0e2bcfca513b12ee963
sarun-xelp <sarun.chuttakulal@xelpmoc.in>
2024-05-10 14:26:01 +0530

    sarundev

--- src/pages/pantry.tsx
+++ src/pages/pantry.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
-import { Button, Form } from 'react-bootstrap'
+import { Button, Form, Table } from 'react-bootstrap'
 import InputText from '../components/inputText/inputText'
-import { pantryFormData } from '../dtos'
 import { createPantry } from '../api/pantry.api'
 import useAuth from '../hooks/useAuth'
 
@@ -36,53 +35,77 @@ const Pantry: React.FC = () => {
 
   return (
     <div>
-      <h1>Pantry</h1>
+      <h1 className='center-heading'>Pantry</h1>
       <Form onSubmit={handleSubmit}>
-        <InputText
-          label='Item Name'
-          name='item_name'
-          placeholder='Enter pantry item name'
-          value={formData.item_name}
-          onChange={handleChange}
-        />
-        <InputText
-          label='Item Description'
-          name='item_description'
-          placeholder='Enter pantry item description'
-          value={formData.item_description}
-          onChange={handleChange}
-        />
-        <div className='Quatity'>
-          <label>Item Quantity:</label>
-          <input
-            type='number'
-            name='item_quantity'
-            placeholder='Enter pantry item quantity'
-            value={formData.item_quantity}
-            onChange={handleChange}
-          />
-        </div>
-        <div className='item_date_of_purchase'>
-          <label>Item Date of Purchase:</label>
-          <input
-            type='date'
-            name='item_date_of_purchase'
-            value={formData.item_date_of_purchase}
-            placeholder='Enter pantry item date of purchase'
-            onChange={handleChange}
-          />
-        </div>
-        <div className='item_expiry_date'>
-          <label>Item Expiry Date:</label>
-          <input
-            type='date'
-            name='item_expiry_date'
-            value={formData.item_expiry_date}
-            placeholder='Enter pantry item expiry date'
-            onChange={handleChange}
-          />
-        </div>
-        <Button type='submit'>Submit</Button>
+        <Table bordered className='custom-table'>
+          <tbody>
+            <InputText
+              label='Name'
+              name='item_name'
+              placeholder='Enter pantry item name'
+              value={formData.item_name}
+              onChange={handleChange}
+            />
+
+            <InputText
+              label='Description'
+              name='item_description'
+              placeholder='Enter pantry item description'
+              value={formData.item_description}
+              onChange={handleChange}
+            />
+            <tr>
+              <td colSpan={2}>
+                <Form.Label>Item Quantity:</Form.Label>
+              </td>
+              <td>
+                <input
+                  type='number'
+                  name='item_quantity'
+                  placeholder='Enter pantry item quantity'
+                  value={formData.item_quantity}
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2}>
+                <Form.Label>Item Date of Purchase:</Form.Label>
+              </td>
+              <td>
+                <input
+                  type='date'
+                  name='item_date_of_purchase'
+                  value={formData.item_date_of_purchase}
+                  placeholder='Enter pantry item date of purchase'
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2}>
+                <Form.Label>Item Expiry Date:</Form.Label>
+              </td>
+              <td>
+                <input
+                  type='date'
+                  name='item_expiry_date'
+                  value={formData.item_expiry_date}
+                  placeholder='Enter pantry item expiry date'
+                  onChange={handleChange}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2}></td>
+              <td colSpan={3}>
+                <Button type='submit' variant='primary'>
+                  Submit
+                </Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
       </Form>
     </div>
   )
